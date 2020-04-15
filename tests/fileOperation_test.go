dc7dbd365b6a480896a088d071ab7246737fff63
Aristidis <ardimopoulos@gmail.com>
2020-04-15 14:42:14 +0300

    Add tests

---
+++ tests/fileOperation_test.go
@@ -0,0 +1,30 @@
+package tests
+
+import (
+	. "../taxiFareCalculator"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestConvertLineToPosition(t *testing.T) {
+	p := ConvertLineToPosition("1", "37.966660", "23.728308", "1405594957")
+
+	fmt.Println(time.Unix(1405594957, 0))
+
+	if p.GetId() != 1 && p.GetLat() != 37.966660 && p.GetLng() != 23.728308 && p.GetTime().Unix() == 1405594957 {
+		t.Error("Wrong values were returned from function")
+	}
+
+	if p == nil {
+		t.Error("Error while returning RidePosition")
+	}
+}
+
+func TestFailedConvertLineToPosition(t *testing.T) {
+	p := ConvertLineToPosition("", "37.966660", "23.728308", "1405594957")
+
+	if p != nil {
+		t.Error("Expected nil RidePosition\n Returned: ", p)
+	}
+}
